c3cb85d8d6d3834ebec8a55eb2ad64247794006d
MinJunKim21 <jinju8012@gmail.com>
2022-07-18 23:56:32 +0900

    making Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,17 +17,21 @@ import {
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
       {/* left */}
-      <div>
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
         />
-        <div>
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6" />
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="flex ml-2 items-center bg-transparent"
+          />
         </div>
       </div>
       {/* center */}
