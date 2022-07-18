3065499d213f6a37fb4f8c267bfe53fafad9407f
MinJunKim21 <jinju8012@gmail.com>
2022-07-18 23:36:57 +0900

    started making Header

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      {/* left */}
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div>
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+      {/* center */}
+      {/* right */}
+    </div>
+  );
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook-Practice</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widget */}
+      </main>
+    </div>
+  );
+}
