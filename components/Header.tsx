e8f4ba14bca179a22083252166f4480737f1cd7b
MinJunKim21 <jinju8012@gmail.com>
2022-07-20 03:49:33 +0900

    finished Header ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* left */}
       <div className="flex items-center">
         <Image
@@ -27,25 +27,34 @@ function Header() {
           layout="fixed"
         />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-          <SearchIcon className="h-6" />
+          <SearchIcon className="h-6 text-gray-600" />
           <input
             type="text"
             placeholder="Search Facebook"
-            className="flex ml-2 items-center bg-transparent"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
           />
         </div>
       </div>
       {/* center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon active Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
+
       {/* right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* <Image /> */}
+        <p className="font-semibold pr-3 whitespace-nowrap">Minjun Kim</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
