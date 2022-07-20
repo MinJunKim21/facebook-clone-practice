eeae2879a6a3ffaf7f72900c42579ce0668c26c0
MinJunKim21 <jinju8012@gmail.com>
2022-07-20 15:41:40 +0900

    nextauth설정하고, SSR로 로그인 여부 확인해서 로그인상태 아니면 로그인페이지로 넘기는거 설정함

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
+import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+
   return (
     <div>
       <Head>
@@ -20,4 +24,16 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+//인덱스 페이지 열리기 전에 로그인 정보를 확인해서 열어주기 위해서 SSR 적용
+export async function getServerSideProps(context) {
+  //get user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
